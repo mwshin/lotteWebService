f18d5ceaf2bf689f4c664a8ebb3d825bb6ef267f
lotte_admin_it <admin_it@lotteshopping.com>
2020-08-12 17:26:58 +0900

    a

--- code-deploy/deploy.sh
+++ code-deploy/deploy.sh
@@ -17,17 +17,21 @@ sudo chown -h jenkins:jenkins ${WAR_BOX_PATH}${ORIGIN_WAR_NAME}
 echo "  > sudo ln -s -f ${WAR_BOX_PATH}${ORIGIN_WAR_NAME} ${TARGET_PATH}"
 sudo ln -s -f ${WAR_BOX_PATH}${ORIGIN_WAR_NAME} ${TARGET_PATH}
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f lotteWebService*)
+#echo "> 현재 실행중인 애플리케이션 pid 확인"
+#CURRENT_PID=$(pgrep -f lotteWebService*)
 
-if [ -z ${CURRENT_PID} ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -9 ${CURRENT_PID}"
-  kill -9 ${CURRENT_PID}
-  sleep 5
-fi
+#if [ -z ${CURRENT_PID} ]
+#then
+#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#  echo "> kill -9 ${CURRENT_PID}"
+#  kill -9 ${CURRENT_PID}
+#  sleep 5
+#fi
 
-echo "> ${ORIGIN_WAR_NAME} 배포"
-nohup java -jar ${ORIGIN_WAR_NAME}
+#echo "> ${ORIGIN_WAR_NAME} 배포"
+#nohup java -jar ${ORIGIN_WAR_NAME}
+
+ORIGIN_JAR=$(readlink /home/jenkins/lotteWebService/lotteWebService.war)
+echo "    > ORIGIN_JAR: ${ORIGIN_JAR}"
+nohup java -jar ${ORIGIN_JAR} 
