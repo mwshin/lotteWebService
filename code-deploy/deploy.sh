16470ee2f2c6c129a5a38e52fce9b170f1a55414
lotte_admin_it <admin_it@lotteshopping.com>
2020-08-12 17:19:44 +0900

    a

--- code-deploy/deploy.sh
+++ code-deploy/deploy.sh
@@ -18,16 +18,16 @@ echo "  > sudo ln -s -f ${WAR_BOX_PATH}${ORIGIN_WAR_NAME} ${TARGET_PATH}"
 sudo ln -s -f ${WAR_BOX_PATH}${ORIGIN_WAR_NAME} ${TARGET_PATH}
 
 echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $ORIGIN_WAR_NAME)
+CURRENT_PID=$(pgrep -f lotteWebService*)
 
-if [ -z $CURRENT_PID ]
+if [ -z ${CURRENT_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -9 ${CURRENT_PID}"
+  kill -9 ${CURRENT_PID}
   sleep 5
 fi
 
-echo "> $ORIGIN_WAR_NAME 배포"
-nohup java -jar $ORIGIN_WAR_NAME
+echo "> ${ORIGIN_WAR_NAME} 배포"
+nohup java -jar ${ORIGIN_WAR_NAME}
